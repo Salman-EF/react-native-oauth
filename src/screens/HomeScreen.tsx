f628b7d13e844f18268257c08a57fc9b25708aba
SALMAN <elfadili.salman@gmail.com>
2022-12-04 15:59:49 +0100

    logout flow

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,46 @@
-import React from "react"
+import React, { useContext } from "react"
+import styled from "styled-components/native";
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
-import { Text } from "react-native";
+import { AuthContext } from "../contexts/AuthContext";
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-const HomeScreen = () => {
+export default function HomeScreen() {
+   const {logout} = useContext(AuthContext)
 
    return (
-      <Text>Hello World</Text>
+      <Container contentContainerStyle={{ flexGrow: 1 }}>
+         <HelloText>Hello World</HelloText>
+         <JoinButton onPress={() => logout()} >
+            <JoinButtonText>Logout</JoinButtonText>
+         </JoinButton>
+      </Container>
    )
 }
-export default HomeScreen;
+
+const Container = styled.ScrollView`
+   flex: 1;
+   background-color: #F6F6F6;
+`
+const HelloText = styled.Text`
+   font-size: 30px;
+   margin-top: 40px;
+   text-align: center;
+`
+const JoinButton = styled.TouchableOpacity`
+   margin-top: auto;
+   padding: 10px 15px;
+   background-color: #4DB6AC;
+   color: #FFFFFF;
+   border-radius: 3px;
+   flex-direction: row;
+`
+const JoinButtonText = styled.Text`
+   flex: 1;
+   text-align: center;
+   font-size: 20px;
+   font-weight: 300;
+   color: #ffffff;
+`
